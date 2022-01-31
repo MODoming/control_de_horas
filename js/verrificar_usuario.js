3bbed94d45e9f0dca48294ef45805814421dbb53
MODoming <aireyluz.md@gmail.com>
2022-01-31 14:31:07 -0300

    agregado csv

--- js/verrificar_usuario.js
+++ js/verrificar_usuario.js
@@ -1,1 +1,23 @@
-var admin = {}
+
+var admin = {usuario: "Maximiliano", pasword: "admin", tipo: "admin"}
+var entradaUsuario = document.querySelector("#usser");
+var entradaPasw = document.querySelector("#pasw");
+var botonIngresar = document.querySelector("#ingresar");
+
+//document.getElementById("controles").style.display = "none";
+botonIngresar.addEventListener("click", function(event){
+    event.preventDefault();
+    if ((admin.usuario === entradaUsuario.value) && (admin.pasword === entradaPasw.value)){
+       if (admin.tipo === "admin"){
+            document.getElementById("saludo").innerHTML="Bienvenid@ " + admin.usuario;
+            document.getElementById("saludo").style.display = "block";
+            document.getElementById("entrada").style.display = "none";
+            document.getElementById("titulo2").style.display = "none";
+            document.getElementById("controles").style.display = "block";
+       } else {
+           alert("Esta ingresando un alumno")
+       }
+    } else {
+        alert("Los datos no son correctos");
+    }});
+
